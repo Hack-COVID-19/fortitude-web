8f4f7da3163f6cb9d336eadaab217c1707f029ff
Ross Ragsdale <ross.ragsdale@gmail.com>
2020-03-29 20:23:58 -0500

    Add patient onboarding form.

--- src/containers/onboard/index.js
+++ src/containers/onboard/index.js
@@ -5,11 +5,11 @@ import { connect } from 'react-redux'
 import {
   setData
 } from '../../modules/iexCloud'
-import { Header, Grid, Button } from 'semantic-ui-react'
+import { Form, Segment, Grid, Button } from 'semantic-ui-react'
 
 const Onboard = props => (
   <div>
-    <h1>Onboarding</h1>
+    <h1>Add New Patient</h1>
     <h4>Use the form below to add a new patient.</h4>
     <p>
       If a patient is no longer able to recieve housecare treatment because of COVID-19 restrictions, you can ask them if they would like to continue recieving treatment remotely.
@@ -17,6 +17,26 @@ const Onboard = props => (
     <p>
       If so, add thir name and phone number to the form below.
     </p>
+
+    <Grid textAlign='center'>
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Form size='large'>
+          <Segment stacked>
+            <Form.Input fluid icon='user' iconPosition='left' placeholder='Name' />
+            <Form.Input
+              fluid
+              icon='phone'
+              iconPosition='left'
+              placeholder='Phone Number'
+              type='phone'
+            />
+            <Button color='teal' fluid size='large'>
+              Add Patient
+            </Button>
+          </Segment>
+        </Form>
+      </Grid.Column>
+    </Grid>
     
     <div className="fill body">
       <Grid>
